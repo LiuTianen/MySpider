1e4bfb4ca1cc8fd76b9f5b48d0a7ae59a50e321b
Tinen <liuyongyi@kobox.tv>
2019-09-26 18:58:26 +0800

    第一次自主尝试修改属于自己的爬虫

---
+++ exercise/wallhaven.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from multiprocessing.dummy import Pool
+import os
+start_url = 'https://wallhaven.cc/latest'
+headers = {
+        "Host": "wallhaven.cc",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
+    }
+def get_source(url):
+    html = requests.get(url,headers=headers).text
+    return html
+# def get_source():
+#     # url = URL = "https://wallhaven.cc/latest"
+#     url = "https://wallhaven.cc/latest"
+#     # for i in range(6):
+#     #     if i > 1:
+#     #         URL = "https://wallhaven.cc/latest?page="+ str(i)
+#     headers = {
+#         "Host": "wallhaven.cc",
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
+#     }
+#     respones = requests.get(url,headers=headers).text
+#     # 解析链接包含的信息
+#     soup = BeautifulSoup(respones, 'lxml')
+#     return soup
+def get_toc(html):
+    # 解析链接包含的信息
+    soup = BeautifulSoup(html, 'lxml')
+    #找到外部的框架
+    img_link_ul = soup.find_all('section',{"class":"thumb-listing-page"})
+    img_list = []
+    for ul in img_link_ul:
+        imgs = ul.find_all('a',{"class":"preview"})
+        #循环查找img标签
+        for img in imgs:
+            #找到链接的标签
+            url = img['href']
+            img_list.append(url)
+        return img_list
+def get_pic_url(pic_html):
+    soup = BeautifulSoup(pic_html, 'lxml')
+    # img_url_loc = soup.find('div',{"ckass":"scrollbox"})
+    # img_name = re.search('data-wallpaper-id="(.*?)"data-wallpaper-width', img_url_loc,re.S)
+    img_url = re.findall('<img id="wallpaper" src="(.*?)" alt')
+    return img_url
+
+def save(img_url):
+    r = requests.get(img_url, stream=True)
+    with open('D:\img\%s'  'wb') as f:
+        for chunk in r.iter_content(chunk_size=128):
+            f.write(chunk)
+    # print('Saved %s' % img_name)
+
+def get_pic(url):
+    pic_html = get_source(url)
+    img_url = get_pic_url(pic_html)
+    save(img_url)
+
+
+
+
+if __name__ == '__main__':
+    toc_html = get_source(start_url)
+    toc_list = get_toc(toc_html)
+    pool = Pool(4)
+    pool.map(get_pic, toc_list)
+
