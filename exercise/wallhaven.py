74256d1140db27090607221de5878764e9c4562e
Liutianen <liutianen@outlook.com>
2019-09-26 22:49:56 +0800

    删除代码

--- exercise/wallhaven.py
+++ exercise/wallhaven.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from multiprocessing.dummy import Pool
-import os
+
 start_url = 'https://wallhaven.cc/latest'
 headers = {
         "Host": "wallhaven.cc",
