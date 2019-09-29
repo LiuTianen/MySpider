cc04004369e9bf2e05d9082bf102e52fe2f0f000
Tinen <liuyongyi@kobox.tv>
2019-09-29 18:58:02 +0800

    添加一个猫眼的案例

---
+++ loading/maoyan.py
@@ -0,0 +1,50 @@
+import requests
+import re
+import time
+import json
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36",
+    "Host": "maoyan.com"
+}
+def get_one_page(url):
+    response =requests.get(url, headers=headers)
+    if response.status_code == 200: #对页面加载做判断
+        return response.text  #加载成功返回页面信息
+    return None #记载失败返回None
+
+
+def parse_one_page(html):
+    #封装需要匹配的元素（compile()函数：返回的是一个匹配对象，单独使用没有任何意义，需要和findall(), search(), match(）搭配使用）
+    pattern = re.compile(
+        '<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>',
+        re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2].strip(),
+            'actor': item[3].strip()[3:] if len(item[3]) > 3 else '',
+            'time': item[4].strip()[5:] if len(item[4]) > 5 else '',
+            'score': item[5].strip() + item[6].strip()
+        }
+
+
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+
+
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+
+
+if __name__ == '__main__':
+    for i in range(10):
+        main(offset=i * 10)
+        time.sleep(1)
