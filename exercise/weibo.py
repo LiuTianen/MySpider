4f0a6a219c302e06784d2462277616adbc68dc67
Tinen <liuyongyi@kobox.tv>
2019-10-08 18:45:33 +0800

    微博链接获取方式的修改

--- exercise/weibo.py
+++ exercise/weibo.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import jsonpath
 start_url = "https://m.weibo.cn/api/container/getIndex?containerid=1076032267821341"
 
 respones = requests.get(start_url).text.encode('utf-8')
@@ -8,15 +8,8 @@ respones = requests.get(start_url).text.encode('utf-8')
 data = json.loads(respones)
 
 comcert = data.get('data').get('cards')
-#需要找到忽略不需要信息的方法
-for Wei in comcert:
-    loc = Wei['itemid']
-    for BO in loc:
-        url = BO['mblog']
-        # ['pics']['large']['url']
-        img_name = url.split('/')[-1]
+# print(comcert)
 
-        r = requests.get(url, stream=True)
-        with open('D:\img\%s' % img_name, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=128):
-                f.write(chunk)
+#jsonpath的用法
+url = jsonpath.jsonpath(comcert,expr='$..avatar_hd')
+print(url)
