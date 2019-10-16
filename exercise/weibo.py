98e845830ce0f740c4ed71718d62aed2d38e8890
Tinen <liuyongyi@kobox.tv>
2019-10-16 18:02:18 +0800

    方法改造，可以获取到链接，但需要过滤

--- exercise/weibo.py
+++ exercise/weibo.py
@@ -1,15 +1,17 @@
 import requests
 import json
 import jsonpath
-start_url = "https://m.weibo.cn/api/container/getIndex?containerid=1076032267821341"
+import re
+from bs4 import BeautifulSoup
+#start_url = "https://m.weibo.cn/api/container/getIndex?containerid=1076032267821341"
+start_url ='https://m.weibo.cn/api/container/getIndex?type=uid&value=2565076590&containerid=1076032565076590'
 
 respones = requests.get(start_url).text.encode('utf-8')
-
 data = json.loads(respones)
-
-comcert = data.get('data').get('cards')
+comcert = data['data']['cards']
 # print(comcert)
-
-#jsonpath的用法
-url = jsonpath.jsonpath(comcert,expr='$..avatar_hd')
+url = re.findall("'scheme': \'(.*?)\'", str(comcert),re.S)
 print(url)
+#jsonpath的用法
+# url = jsonpath.jsonpath(comcert,expr='$..avatar_hd')
+# print(url)
